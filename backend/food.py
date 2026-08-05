1cb2502c3eb67080f7bd131fe03754668b3c0860
Quarkus-Quirinitus <jasmin@romeyke.de>
2026-08-05 20:56:56 +0200

    changed sth

--- backend/food.py
+++ backend/food.py
@@ -1,6 +1,5 @@
 from backend.animal_old import AnimalType
 from typing import List
-from simulation_engine import get_current_day
 
 class Food:
     def __init__(self, name: str, animalTypes: List[AnimalType], shelfLife: int, pricePerKg: int):
@@ -10,11 +9,24 @@ class Food:
         self.pricePerKg = pricePerKg
 
 
+class Meat(Food):
+    def __init__(self):
+        super().__init__("Meat", [AnimalType.CARNIVORE], 10, 3)
+
+class Hay(Food):
+    def __init__(self):
+        super().__init__("Hay", [AnimalType.HERBIVORE], 25, 1)
+
+class Fish(Food):
+    def __init__(self):
+        super().__init__("Fish", [AnimalType.CARNIVORE], 7, 5)
+
+
 
 class FoodItem:
-    def __init__(self, type: Food, weight: int):
+    def __init__(self, type: Food, weight: int, elapsedDays: int):
         self.type = type
         self.weight = weight
-        self.bestBefore = get_current_day() + self.type.shelfLife
+        self.bestBefore = elapsedDays + self.type.shelfLife
         self.price = self.type.pricePerKg * self.weight
 
